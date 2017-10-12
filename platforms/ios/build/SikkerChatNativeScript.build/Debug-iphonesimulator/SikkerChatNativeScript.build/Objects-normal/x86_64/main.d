4f09f17eae984bd4493f21572e81f1a4f2f6371c
Scott J. Roberts <Scott.John.Roberts@ibm.com>
2017-10-11 21:10:00 -0400

    Initial commit

---
+++ platforms/ios/build/SikkerChatNativeScript.build/Debug-iphonesimulator/SikkerChatNativeScript.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/SikkerChatNativeScript/SikkerChatNativeScript-Prefix.pch \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/main.m \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/TNSDebugging.h \
+  /Users/Scott.John.Roberts@ibm.com/Desktop/Mobile\ Computing/APP/SikkerChatNativeScript/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
