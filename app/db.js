208a9e459479c2233c8be8f956aad34c0b31dc51
Scott J. Roberts <Scott.John.Roberts@ibm.com>
2017-10-18 14:22:54 -0400

    Update Functionality
    WIP

--- app/db.js
+++ app/db.js
@@ -20,7 +20,7 @@ var da = {
 
 function addChat(data, chatName) {
   var uniqueID = chatName;
-  testDB.insert(data, uniqueID, function (err, body) {
+  testDB.insert(data, uniqueID, function(err, body) {
     if (!err) {
       console.log('added to database with id: ', uniqueID);
       return true;
@@ -29,7 +29,7 @@ function addChat(data, chatName) {
 }
 
 function getRevNum(chatName) {
-  testDB.get('dirtyPeteScottyR', function (err, body) {
+  testDB.get('dirtyPeteScottyR', function(err, body) {
     if (!err) {
       console.log(body._rev);
       return body._rev;
@@ -37,21 +37,103 @@ function getRevNum(chatName) {
   });
 }
 
+function getMessages(chatName) {
+  return new Promise((resolve, reject) => {
+    testDB.get(chatName, function(err, body) {
+      if (!err) {
+        //console.log(body.messages);
+        var messageBody = body.messages;
+        if (messageBody) {
+          resolve(messageBody);
+        } else {
+          //console.log('No messages to retreive, check spelling');
+        }
+      } else {reject('ERROR');}
+    });});
+}
+
 function getMessageFrom(chatName, userName) {
-  testDB.get(chatName, function (err, body) {
-    if (!err) {
-      console.log(body.messages[userName]);
-      var messageFrom = body.messages[userName];
-      if (messageFrom) {
-        return messageFrom;
-      } else {
-        console.log('User not found, check spelling');
+  return new Promise((resolve, reject) => {
+    testDB.get(chatName, function(err, body) {
+      if (!err) {
+        //console.log(body.messages[userName]);
+        var messageFrom = body.messages[userName];
+        if (messageFrom) {
+          resolve(messageFrom);
+        } else {
+          reject('User not found, check spelling');
+        }
+      } else {console.log('ERROR');}
+    });
+  });}
+
+function getChatInfo(chatName) {
+  return new Promise((resolve, reject) => {
+    testDB.get(chatName, function (err, body) {
+      if (!err) {
+        console.log('inside');
+        var info = body;
+        if (body) {resolve(body);} else {reject('FUCKED UP');}
       }
-    } else {console.log('ERROR');}
+
+    });
+  });
+}
+
+/*
+  alice.insert({ _id: 'myid', _rev: '1-23202479633c2b380f79507a776743d5', happy: false }, function(err, body) {
+    if (!err)
+      console.log(body)
+  })
+*/
+function jBuilder(jMess, newMess, userNamte) {
+  //build your initial message
+
+  //add it in
+
+  //reverse it all
+
+  return newSon;
+}
+function addMessage(chatName, mess, userName) {
+  var rev = '';
+  getChatInfo(chatName).then((data) => {
+    rev = data._rev;
+  });
+
+  //builds the message as one new JSON object
+  getMessages(chatName).then((data) => {
+    var a = JSON.stringify(data);
+    var b = a.replace('{', '');
+    b = b.replace('}', '');
+
+    //console.log('b is', b);
+    var messArray = b.split(',');
+    var createObj = '\"' + userName + '\"' + ':' + '\"' + mess + '\"';
+    messArray.push(createObj);
+    messArray.reverse();
+
+    //console.log(messArray);
+    var newJSON = '{';
+    while (messArray.length > 0) {
+      newJSON = newJSON + messArray.pop() + ',' + '\n';
+    }
+
+    newJSON = newJSON + '}';
+
+    testDB.insert({ _id: chatName, _rev: rev, messages: newJSON }, function (err, body) {
+      if (!err) {
+        //good to go
+        console.log('change set');
+      }
+    });
   });
 }
 
 //removeChat('d38bcec8cdfe71cc0f52604f4f000ce5', '2-165cf56d3bd4d973599d08ae172209f9');
 //addChat(da, 'dirtyPeteScottyR');
-//getChatInfo('dirtyPeteScottyR');
-getMessageFrom('dirtyPeteScottyR', 'scottyR');
+//getChatInfo('dirtyPeteScottyR').then((data) => {console.log(data._rev);});
+
+//getMessageFrom('dirtyPeteScottyR', 'scottyR');
+//getMessages('dirtyPeteScottyR');
+addMessage('dirtyPeteScottyR', 'FUCK I THINK ITS WORKING', 'DirtyPete');
