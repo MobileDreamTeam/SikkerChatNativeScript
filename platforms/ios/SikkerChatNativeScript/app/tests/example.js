143d4397968c1dc08f39e7318ce26d330d83deb6
Scott J. Roberts <Scott.John.Roberts@ibm.com>
2017-10-18 21:25:20 -0400

    Test Suite stuff

---
+++ platforms/ios/SikkerChatNativeScript/app/tests/example.js
@@ -0,0 +1,22 @@
+const should = require('should');
+const request = require('supertest');
+const db = require('../db.js');
+const sinon = require('sinon');
+
+describe('Database', function () {
+	describe('addChat()', function () {
+		it('should return -1 when the value is not present', function () {
+			assert.equal(-1, [1,2,3].indexOf(5));
+			assert.equal(-1, [1,2,3].indexOf(0));
+		});
+	});
+});
+
+//removeChat('d38bcec8cdfe71cc0f52604f4f000ce5', '2-165cf56d3bd4d973599d08ae172209f9');
+//removeChat('dirtyPeteScottyR', '21-df7a0837740c673026bbeaceb994aa76');
+//
+//getChatInfo('dirtyPeteScottyR').then((data) => {console.log(data._rev);});
+
+//getMessageFrom('dirtyPeteScottyR', 'scottyR');
+//getMessages('dirtyPeteScottyR');
+//addMessage('dirtyPeteScottyR', 'FUCK I THINK ITS WORKING', 'DirtyPete');
